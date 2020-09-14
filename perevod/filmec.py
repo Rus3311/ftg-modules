5be3f88a8d49f4a3cc47e708317788b4ed001cc9
Rus3311 <70165313+Rus3311@users.noreply.github.com>
2020-09-15 00:40:20 +0500

    Muzika

--- perevod/filmec.py
+++ perevod/filmec.py
@@ -1,4 +1,4 @@
-#by @laciamemeframe
+# Russian tts for Friendly-telegram: .rtts
 
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
@@ -6,46 +6,68 @@ from .. import loader, utils
 
 
 def register(cb):
-	cb(nudes2dMod())
+    cb(RttsMod())
 
 
-class nudes2dMod(loader.Module):
-	"""Хентай картинки из @murglar_bot"""
+class RttsMod(loader.Module):
+    """Rtts - гениально простое решение для tts на русском языке"""
 
-	strings = {'name': 'Хентай'}
+    strings = {'name': 'Rtts'}
 
-	def __init__(self):
-		self.name = self.strings['name']
-		self._me = None
-		self._ratelimit = []
+    def __init__(self):
+        self.name = self.strings['name']
+        self._me = None
+        self._ratelimit = []
 
-	async def client_ready(self, client, db):
-		self._db = db
-		self._client = client
-		self.me = await client.get_me()
+    async def client_ready(self, client, db):
+        self._db = db
+        self._client = client
+        self.me = await client.get_me()
 
-	async def hentaicmd(self, event):
-         """.hentai"""
-         user_msg = """{}""".format(utils.get_args_raw(event))
-         global text
-         text = False
-         if event.fwd_from:
-             return
-             self_mess = True
-             if not user_msg:
-                 return 
-         chat = '@playplay_bot'
-         await event.edit('<code>Обработка</code>')
-         async with event.client.conversation(chat) as conv:
-             try:
-                 response = conv.wait_event(events.NewMessage(incoming=True,
-                                                              from_users=678543122))
-                 await event.client.send_message(chat, '/muz ' +user_msg)
-                 response = await response
-             except YouBlockedUserError:
-                 await event.reply('<code>Разблокируй @playplay_bot</code>')
-                 return
-             await event.delete()
-             await event.client.send_file(event.to_id, response.media)
-    
-	
+    async def rttscmd(self, event):
+        """.rtts {текст} или .rtts как ответ на смс;
+        .rtts {текст} как ответ - ответ голосом на смс"""
+        user_msg = """{}""".format(utils.get_args_raw(event))
+        global reply_and_text
+        reply_and_text = False
+        if event.fwd_from:
+            return
+        if not event.reply_to_msg_id:
+            self_mess = True
+            if not user_msg:
+                await event.edit('<code>Вы должны или написать шото, '
+                                 'или ответить на шото</code>')
+                return
+        elif event.reply_to_msg_id and user_msg:
+            reply_message = await event.get_reply_message()
+            reply_and_text = True
+            self_mess = True
+        elif event.reply_to_msg_id:
+            reply_message = await event.get_reply_message()
+            self_mess = False
+            if not reply_message.text:
+                await event.edit('<code>Ты на текст должен ответить, диб*ил</code>')
+                return
+        chat = '@playplay_bot'
+        await event.edit('<code>Происходит магия Деменкопа</code>')
+        async with event.client.conversation(chat) as conv:
+            try:
+                response = conv.wait_event(events.NewMessage(incoming=True,
+                                                             from_users=678543122))
+                if not self_mess:
+                    await event.client.forward_messages(chat, reply_message)
+                else:
+                    await event.client.send_message(chat, user_msg)
+                response = await response
+            except YouBlockedUserError:
+                await event.reply('<code>Разблокируй бот, ибо магия не произойдёт</code>')
+                return
+            if response.text:
+                await event.edit('<code>Бот ответил не медиа форматом, попробуйте снова</code>')
+                return
+            await event.delete()
+            if reply_and_text:
+                await event.client.send_message(event.chat_id, response.message,
+                                                reply_to=reply_message.id)
+            else:
+                await event.client.send_message(event.chat_id, response.message)
