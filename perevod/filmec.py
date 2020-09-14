74ce8c5e539b72bed7ae19fe10f33d67654cc628
Rus3311 <70165313+Rus3311@users.noreply.github.com>
2020-09-14 22:02:46 +0500

    Muz.ry

--- perevod/filmec.py
+++ perevod/filmec.py
@@ -1,103 +1,56 @@
-"""Get details of a series or movie.
-Command: 
-.tv <название сериала>
-.film <название фильма>
-  © [cHAuHaN](http://t.me/amnd33p)"""
-
-import requests
-import bs4
-import re
 from telethon import events
-from uniborg.util import *
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from .. import loader, utils
 
-@borg.on(events.NewMessage(outgoing=True,pattern='.film (.*)'))
 
-@borg.on(events.MessageEdited(outgoing=True,pattern='.film (.*)'))
+def register(cb):
+	cb(VKMMod())
 
-@borg.on(events.NewMessage(outgoing=True,pattern='.tv (.*)'))
 
-@borg.on(events.MessageEdited(outgoing=True,pattern='.tv (.*)'))
+class Lines50Mod(loader.Module):
+	"""Find music via @vkm_bot"""
 
-async def imdb(e):
-    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        try:
-            movie_name = e.pattern_match.group(1)
-            remove_space = movie_name.split(' ')
-            final_name = '+'.join(remove_space)
-            page = requests.get("https://www.imdb.com/find?ref_=nv_sr_fn&q="+final_name+"&s=all")
-            lnk = str(page.status_code)
-            soup = bs4.BeautifulSoup(page.content,'lxml')
-            odds = soup.findAll("tr","odd")
-            mov_title = odds[0].findNext('td').findNext('td').text
-            mov_link = "http://www.imdb.com/"+odds[0].findNext('td').findNext('td').a['href']
-            page1 = requests.get(mov_link)
-            soup = bs4.BeautifulSoup(page1.content,'lxml')
-            if soup.find('div','poster'):
-              poster = soup.find('div','poster').img['src']
-            else:
-              poster = ''
-            if soup.find('div','title_wrapper'):
-              movie_title = soup.find('div','title_wrapper').findNext('h1').text
-              pg = soup.find('div','title_wrapper').findNext('div').text
-              mov_details = re.sub(r'\s+',' ',pg)
-            else:
-              mov_details = ''
-            credits = soup.findAll('div', 'credit_summary_item')
-            if len(credits)==1:
-              director = credits[0].a.text
-              writer = 'Недоступно 😢'
-              stars = 'Недоступно 😢'
-            elif len(credits)>2:
-              director = credits[0].a.text
-              writer = credits[1].a.text
-              actors = []
-              for x in credits[2].findAll('a'):
-                actors.append(x.text)
-              actors.pop()
-              stars = actors[0]+','+actors[1]+','+actors[2]
-            else:
-              director = credits[0].a.text
-              writer = 'Недоступно 😢'
-              actors = []
-              for x in credits[1].findAll('a'):
-                actors.append(x.text)
-              actors.pop()
-              stars = actors[0]+','+actors[1]+','+actors[2]
-            if soup.find('div', "inline canwrap"):
-              story_line = soup.find('div', "inline canwrap").findAll('p')[0].text
-            else:
-              story_line = 'Недоступно 😢'
-            info = soup.findAll('div', "txt-block")
-            if info:
-              mov_country = []
-              mov_language = []
-              for node in info:
-                a = node.findAll('a')
-                for i in a:
-                  if "country_of_origin" in i['href']:
-                    mov_country.append(i.text)
-                  elif "primary_language" in i['href']:
-                    mov_language.append(i.text)
-            if soup.findAll('div',"ratingValue"):
-              for r in soup.findAll('div',"ratingValue"):
-                mov_rating = r.strong['title']
-            else:
-              mov_rating = 'Недоступно 😢'
-            await e.edit('<b>Название : </b><code>'+mov_title+
-                  '<a href='+poster+'> ‏‏‎ </a>'
-                  '</code>\n<code>'+mov_details+
-                  '</code>\n<b>Оценки : </b><code>'+mov_rating+
-                  '</code>\n<b>Страна : </b><code>'+mov_country[0]+
-                  '</code>\n<b>Язык : </b><code>'+mov_language[0]+
-                  '</code>\n<b>Режиссёр : </b><code>'+director+
-                  '</code>\n<b>Сценарист : </b><code>'+writer+
-                  '</code>\n<b>Звёзды : </b><code>'+stars+
-                  '</code>\n<b>IMDB ссылка : </b>'+mov_link+
-                  '\n\n<b>Держи пёселя : </b> <a href="https://www.unn.com.ua/uploads/assets/images/%D0%92%D0%B5%D0%BB%D1%8C%D1%88-%D0%BA%D0%BE%D1%80%D0%B3%D0%B8%20%D0%BF%D0%B5%D0%BC%D0%B1%D1%80%D0%BE%D0%BA.jpg">Пёсель</a>'+
-                  '\n\n===> Приятного дня! <===',
-                  link_preview = True , parse_mode = 'HTML'
-                  )
-        except IndexError:
-            await e.edit("Хьюстон, результатов не найдено. Пожалуйста, введи ** Правильное название фильма / сериала **")
-        except Exception as err:
-            await e.edit("Хьюстон, произошла некая ошибка:- "+str(err))
+	strings = {'name': 'vkm_bot'}
+
+	def __init__(self):
+		self.name = self.strings['name']
+		self._me = None
+		self._ratelimit = []
+
+	async def client_ready(self, client, db):
+		self._db = db
+		self._client = client
+		self.me = await client.get_me()
+
+	async def linescmd(self, message):
+		""".muz <text>"""
+		
+		reply = await message.get_reply_message()
+		if not reply:
+			await message.edit("text")
+			return
+		try:
+			photo = reply.media.photo
+		except:
+			await message.edit("text")
+			return
+		
+		
+				
+				
+		chat = '@vkm_bot'
+		await message.edit('@vkm_bot <code>in process...</code>')
+		async with message.client.conversation(chat) as conv:
+			try:
+				response = conv.wait_event(events.NewMessage(incoming=True, from_users=1120861844))
+				
+				await message.client.send_file(chat, photo)
+				
+				response = await response
+			except YouBlockedUserError:
+				await message.reply('<code>Unblock</code> @vkm_bot')
+				return
+
+			await message.delete()
+			await message.client.send_file(message.to_id, response.media)
+			
