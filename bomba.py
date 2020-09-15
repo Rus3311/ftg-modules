8a0e2ee40b9de96103d137a73b98350b3b77606b
Rus3311 <70165313+Rus3311@users.noreply.github.com>
2020-09-15 09:21:24 +0500

    Cutter

--- bomba.py
+++ bomba.py
@@ -1,31 +1,56 @@
 from telethon import events
-import asyncio
-import os
-import sys
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from .. import loader, utils
 
 
-@borg.on(events.NewMessage(pattern=r"\.bombs", outgoing=True))
-async def _(event):
-    if event.fwd_from:
-        return
-       
- 
-    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n")
-    await asyncio.sleep(0.5)
-    await event.edit("💣💣💣💣 \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n")
-    await asyncio.sleep(0.5)
-    await event.edit("▪️▪️▪️▪️ \n💣💣💣💣 \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n")
-    await asyncio.sleep(0.5)
-    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n💣💣💣💣 \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n")
-    await asyncio.sleep(0.5)
-    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n💣💣💣💣 \n▪️▪️▪️▪️ \n")
-    await asyncio.sleep(0.5)
-    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n💣💣💣💣 \n")
-    await asyncio.sleep(1)
-    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n💥💥💥💥 \n")
-    await asyncio.sleep(0.5)
-    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n💥💥💥💥 \n💥💥💥💥 \n")
-    await asyncio.sleep(0.5)
-    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n😵😵😵😵 \n")
-    await asyncio.sleep(0.5)
-    await event.edit("БОМБИТ С ТЕБЯ!")
+def register(cb):
+	cb(CutMod())
+
+
+class Lines50Mod(loader.Module):
+	"""Cut music with 50 lines via @audiocutterbot"""
+
+	strings = {'name': 'audiocutter'}
+
+	def __init__(self):
+		self.name = self.strings['name']
+		self._me = None
+		self._ratelimit = []
+
+	async def client_ready(self, client, db):
+		self._db = db
+		self._client = client
+		self.me = await client.get_me()
+
+	async def linescmd(self, message):
+		""".cut <reply to music>"""
+		
+		reply = await message.get_reply_message()
+		if not reply:
+			await message.edit("reply to music")
+			return
+		try:
+			photo = reply.media.photo
+		except:
+			await message.edit("reply to music only")
+			return
+		
+		
+				
+				
+		chat = '@audiocutterbot'
+		await message.edit('... <code>in process...</code>')
+		async with message.client.conversation(chat) as conv:
+			try:
+				response = conv.wait_event(events.NewMessage(incoming=True, from_users=438382295))
+				
+				await message.client.send_file(chat, music)
+				
+				response = await response
+			except YouBlockedUserError:
+				await message.reply('<code>Unblock</code> @audiocutterbot')
+				return
+
+			await message.delete()
+			await message.client.send_file(message.to_id, response.media)
+			
