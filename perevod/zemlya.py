d12786b5284f182c99a3d2c8649db507b70d285f
Rus3311 <70165313+Rus3311@users.noreply.github.com>
2020-12-18 16:32:29 +0000

    Suka

--- perevod/zemlya.py
+++ perevod/zemlya.py
@@ -3,11 +3,11 @@ import asyncio
 from collections import deque
 
 
-@borg.on(events.NewMessage(pattern=r"\.earth", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.suka", outgoing=True))
 async def _(event):
 	if event.fwd_from:
 		return
-	deq = deque(list("🌏🌍🌎🌎🌍🌏🌍🌎"))
+	deq = deque(list("🌕т🌔ы🌓с🌒у🌑к🌘а🌗"))
 	for _ in range(48):
 		await asyncio.sleep(0.1)
 		await event.edit("".join(deq))
